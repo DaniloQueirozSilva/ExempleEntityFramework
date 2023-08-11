916f211e9f53af5463a3cdf91010b6c0dd05722d
Danilo Queiroz <daniloqueirozsilva05@gmail.com>
2023-08-11 12:39:09 -0300

    Adicionar arquivos de projeto.

---
+++ Data/BlogDataContext.cs
@@ -0,0 +1,45 @@
+﻿using Blog.Models;
+using BlogEF.Mappings;
+using BlogEF.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Net.Security;
+
+namespace BlogEF.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }             
+        public DbSet<User> Users { get; set; }
+        public DbSet<PostWithTagsCount> PostWithTagsCounts { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Persist Security Info=False;Integrated Security=true;Initial Catalog=Blog;server=DESKTOP-3PVVSJJ\\SERVERSQL");
+
+           
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
+            modelBuilder.ApplyConfiguration(new PostMap());
+
+
+            modelBuilder.Entity<PostWithTagsCount>(x =>
+            {
+                x.ToSqlQuery(@"
+                SELECT
+                    [Title],
+                SELECT COUNT ([Id]) FROM [Tags] WHERE [PostId] = [Id]
+                        AS [Count]
+                FROM
+                    [Posts]");
+            }
+            );
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using Blog.Models;
+using BlogEF.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace BlogEF
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            using var context = new BlogDataContext();
+
+            var user = new User
+            {
+                Name = "Danilo Queiroz",
+                Slug = "daniloqueiroz",
+                GitHub = "daniloDev",
+                Email = "daniloqueiroz@gmail.com",
+                Bio = "1x Microsfot MVP",
+                Image = "C:/DANILO",
+                PasswordHash = "12345",
+            };
+
+            //    var category = new Category
+            //    {
+            //        Name = "Backend",
+            //        Slug = "backend"
+            //    };
+
+            //    var post = new Post
+            //    {
+            //        Author = user,
+            //        Category = category,
+            //        Body = "<p>Hello World</p>",
+            //        Slug = "comecando-com-ef-core",
+            //        Summary = "Neste artigo vamos aprender EF Core",
+            //        Title = "Começando com EF Core",
+            //        CreateDate = DateTime.Now,
+            //        LastUpdateDate = DateTime.Now,
+            //    };
+
+            //    context.Posts.Add(post);
+            //    context.SaveChanges();
+            //}
+
+            //var post = context
+            //    .Posts
+            //    .Include(x => x.Author)
+            //    .Include(x => x.Category)
+            //    .OrderByDescending(x => x.LastUpdateDate)
+            //    .FirstOrDefault();
+
+            //post.Author.Name = "Danilo Teste";
+
+            //context.Posts.Update(post);
+            //context.SaveChanges();
+
+            //context.Users.Add(new User {
+
+            //    Name = "Davi Queiroz",
+            //    Slug = "daviqueiroz",
+            //    Email = "daviqueiroz@gmail.com",
+            //    Bio = "3x Microsfot MVP",
+            //    Image = "C:/Davi",
+            //    PasswordHash = "12345"
+            //});
+
+            //context.SaveChanges();
+
+            //var user = context.Users.FirstOrDefault();
+
+
+            //var post = new Post
+            //{
+            //    Author = user,
+            //    Body = "Meu artigo",
+            //    Category = new Category
+            //    {
+            //        Name = "FrontEnd",
+            //        Slug = "frontend"
+            //    },
+            //    CreateDate = DateTime.Now,
+            //    // LastUpdateDate=
+            //    Slug = "meu-artigo",
+            //    Summary = "Neste artigo vamos conferir....",
+            //    // Tags=Null
+            //    Title = "Meu artigo"
+
+            //};
+            
+
+            context.Users.Add(user);
+            context.SaveChanges();
+
+
+            //var posts = context.Posts
+            //    .Include(x => x.Author)
+            //        .ThenInclude(x => x.Roles); //pegando itens da role do objeto author
+
+            var posts = context.PostWithTagsCounts.FirstOrDefault();
+
+        }
+    }
+}
